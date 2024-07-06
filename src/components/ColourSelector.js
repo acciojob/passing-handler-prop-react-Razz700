132f9cabcfedb0997e7d7d991fede260aba17a08
Razzakh <mrazzakh123@gmail.com>
2024-07-06 10:05:04 +0530

    added data attributes

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -4,7 +4,7 @@ const ColourSelector = (props) => {
   const { config, selectNextBackground } = props
   const { background } = config.background;
   return (
-    <button id={config.label} className={config.classname/* classname should come here */} onClick={() =>{ selectNextBackground({background: config.background});
+    <button data-testid={config.label} className={config.classname/* classname should come here */} onClick={() =>{ selectNextBackground({background: config.background});
                                                                                     }}>
       {config.label/* label should come here */}
     </button>
