63a8876856e6b8fa7c8bf28e5a49a7dd03b780a5
Razzakh <mrazzakh123@gmail.com>
2024-07-06 12:06:46 +0530

    no

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -3,17 +3,15 @@ import '../styles/Child.css'
 const Selection = (props) => {
  //   console.log(props.bg.background);
     let [bg,setbg]=useState('');
-    let [dat,setdat]=useState('');
   const handleclick=()=>{
     setbg(props.bg.background);
-    if (bg=='rgb(34, 193, 195)') {
-        setdat('blue');
-      }else if(bg=='rgb(221, 112, 18)'){
-        setdat('orange');
-      }else if(bg=='rgb(44, 209, 88)'){setdat('green');}
   }
-
-
+  let dat='';
+  if (bg=='rgb(34, 193, 195)') {
+    dat='blue';
+  }else if(bg=='rgb(221, 112, 18)'){
+    dat='orange'
+  }else if(bg=='rgb(44, 209, 88)'){dat='green'}
   return (
          <div className='fix-box' data-testid={dat} style={{backgroundColor:bg}} onClick={
             handleclick
